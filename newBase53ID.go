fc1064c6e680b23341f01b75590020a43a19559c
The That It <10658503+1f604@users.noreply.github.com>
2024-01-02 13:11:09 +0000

    Create newBase53ID.go

---
+++ newBase53ID.go
@@ -0,0 +1,24 @@
+// Run this with go run ./newBase53ID.go
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/1f604/util"
+)
+
+func main() {
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) != 1 {
+		fmt.Println("Usage: gen_random_id [length]")
+		os.Exit(1)
+	}
+	length, err := util.String_to_int64(os.Args[1])
+	util.Check_err(err)
+
+	b53m := util.NewBase53IDManager()
+	id, _ := b53m.B53_generate_random_Base53ID(int(length))
+
+	fmt.Println(id)
+}
